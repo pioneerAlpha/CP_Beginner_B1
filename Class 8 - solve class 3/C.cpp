7a93ded4d9e75c0ec36f49ddf83435e3de34a8c9
tbssajal <45002968+tbssajal@users.noreply.github.com>
2020-07-08 17:19:14 +0600

    Create C.cpp

---
+++ Class 8 - solve class 3/C.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define endl "\n"
+#define ll long long
+#define N ((int)(1e5 + 5))
+#define MOD ((int)(1e9 + 7))
+
+using namespace std;
+
+
+/*fast io
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+*/
+
+///sieve of eratosthenes
+int status[(N/32)+2];  /// mx = maximum prime
+//vector < int > prime;
+bool Check(int n,int pos){
+    return (bool)(n & (1<<pos));
+}
+int Set(int n,int pos){
+    return (n | (1<<pos));
+}
+void sieve(int n){
+     for(int i = 3; i*i <= n; i += 2){
+		 if( Check(status[i>>5],i&31)==0){
+	 		 for(int j = i*i; j <= n; j += (i<<1) ){
+				 status[j>>5]=Set(status[j>>5],j & 31);
+	 		 }
+		 }
+	 }
+//	 prime.push_back(2);
+//	 for(int i=3;i<=n;i+=2) if( Check(status[i>>5],i&31)==0) prime.push_back(i);
+}
+
+
+
+int main()
+{
+    fastio;
+    sieve(1 + 1e5);
+    int n;
+    cin>>n;
+    if(n < 3) cout<<"1\n1 ";
+    else cout<<"2\n1 ";
+    for(int i = 3 ; i<=n+1 ; i++){
+        if( Check(status[i>>5],i&31) == 0 && i%2 == 1 ) cout<<"1 ";
+        else cout<<"2 ";
+    }
+    return 0;
+}
