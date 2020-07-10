d9a6cea8fbe8a93000603bc95a54246d8d4ac2d2
tbssajal <45002968+tbssajal@users.noreply.github.com>
2020-07-11 01:52:34 +0600

    Create totient function (sqrt(n)).cpp

---
+++ Class 9 - totient function, combinatorics, recursion, backtracking/totient function (sqrt(n)).cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+/// calculate phi for a given number (given number <= 1e12)
+
+int main()
+{
+    ll n;
+    cin>>n;
+    ll ans = n;
+    /// phi ( n ) = n*(p-1)/p * (q-1)/q * (r-1)/r  where n has prime factor p , q , r;
+    vector < ll > prime ; /// this vector stores all the prime number from 1 to sqrt(n) (  that is 1e6 at most )
+    for(ll p:prime){
+        if(p*p>n) break;
+        if(n%p == 0){
+            ans /= p;
+            ans *= p-1;
+            while(n%p == 0) n /= p;
+        }
+    }
+    if(n>1){
+        ans /= n;
+        ans *= n;
+    }
+    cout<<ans<<endl;
+    /// complexity : O(sqrt(n))
+    return 0;
+}
