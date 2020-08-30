75d1e74fa37a9836d88c7d4ecda444bf7eb4263a
tbssajal <45002968+tbssajal@users.noreply.github.com>
2020-08-30 14:47:40 +0600

    Create coin change (1st variation).cpp

---
+++ Class 24 - solve class 12, coin change, subset sum problem/coin change (1st variation).cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define N ((int)1e5 + 5)
+#define MOD ((int)1e9 + 7)
+#define MAX ((int)1e9 + 7)
+#define MAXL ((ll)1e18 + 7)
+#define MAXP ((int)1e3 + 7)
+#define thr 1e-8
+#define pi acos(-1)  /// pi = acos ( -1 )
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define endl "\n"
+
+using namespace std;
+
+int dpp[105][100005] , coin[105] , n;
+
+/// 'yes' 'no' variation
+
+bool call(int idx , int rem)
+{
+    if(rem < 0) return 0;
+    if(rem == 0) return 1;
+    if(idx > n) return 0;
+    if(dpp[idx][rem] != -1) return dpp[idx][rem];
+    return dpp[idx][rem] = call(idx+1,rem) | call(idx+1,rem - coin[idx]);
+}
+
+int main()
+{
+    fastio;
+    int val;
+    cin>>n>>val;
+    for(int i = 1 ; i<=n ; i++) cin>>coin[i];
+    memset(dpp,-1,sizeof dpp);
+    if(call(1,val)) cout<<"YES\n";
+    else cout<<"NO\n";
+    return 0;
+}
