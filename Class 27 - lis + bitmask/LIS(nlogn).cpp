2358fcadd26a32f30427fa01b7bdb825889782c6
tbssajal <45002968+tbssajal@users.noreply.github.com>
2020-09-10 04:31:29 +0600

    Create LIS(nlogn).cpp

---
+++ Class 27 - lis + bitmask/LIS(nlogn).cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define N ((int)1e5 + 5)
+#define MOD ((int)1e8 + 7)
+#define MAX ((int)1e9 + 7)
+#define MAXL ((ll)1e10 + 7)
+#define MAXP ((int)1e6 + 7)
+#define thr 1e-8
+#define pi acos(-1)  /// pi = acos ( -1 )
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
+#define endl "\n"
+
+using namespace std;
+
+int arr[N] , dpp[N] , val[N];
+
+int main()
+{
+
+    int n;
+    cin>>n;
+    for(int i = 1 ; i<=n ; i++) cin>>arr[i];
+    val[0] = -MAX;
+    for(int i = 1 ; i<=n ; i++) val[i] = MAX;
+    for(int i = 1 ; i<=n ; i++){
+
+        int lef = 0 , rig = n;
+        while(lef < rig){
+            int mid = (lef + rig)>>1;
+            if(val[mid] < arr[i]) lef = mid+1;
+            else rig = mid;
+        }
+        dpp[i] = rig;
+        val[rig] = arr[i];
+    }
+    int lis = 0;
+    for(int i = 1 ; i<=n ; i++) lis = max(lis , dpp[i]);
+    cout<<lis<<endl;
+    for(int i = 1 ; i<=lis ; i++) cout<<val[i]<<" ";
+    cout<<endl;
+    return 0;
+}
